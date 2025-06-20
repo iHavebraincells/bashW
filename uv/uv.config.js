0ff7e3ca726ab72fb0d96eee329b3f8b84287262
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 01:09:50 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,10 +1,6 @@
-
 self.__uv$config = {
-  prefix: '/uv/',
-  bare: 'https://raw.githubusercontent.com/titaniumnetwork-dev/bare/main/',
-  encodeUrl: function (url) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(url);
-    return btoa(String.fromCharCode(...data));
-  }
+  prefix: '/bashW/uv/',
+  bare: 'https://bare.pages.dev',
+  encodeUrl: (url) => btoa(url),
+  decodeUrl: (url) => atob(url)
 };
