bf26676bea30b9e1f4749727c3c0503340b57d18
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 01:08:02 -0400

    Update uv.bundle.js

--- uv/uv.bundle.js
+++ uv/uv.bundle.js
@@ -1,1 +1,13 @@
-// UV bundle JS placeholder
+import { UVServiceWorker } from './uv.sw.js';
+import { UVHandler } from './uv.handler.js';
+
+self.UV = {
+  config: {
+    prefix: '/bashW/uv/',
+    bare: 'https://bare.pages.dev',
+    encodeUrl: (url) => btoa(url),
+    decodeUrl: (url) => atob(url)
+  },
+  handler: UVHandler,
+  serviceWorker: UVServiceWorker
+};
